67e2743fbf083c0ffe0f47c1d1e0a3a5cdd2800a
Mauro Navarro <mauronav006@gmail.com>
2026-03-08 00:13:37 -0600

    Se agrega informe pdf y comentarios al main

--- Semana1/Ejercicio1/main.m
+++ Semana1/Ejercicio1/main.m
@@ -12,6 +12,15 @@ xlabel('d');
 ylabel('F(d)');
 title('Funcion F(d)');
 
-% Encontrar la raiz con fzero
+#{
+================================================================================
+Encontrar la raiz con fzero
+
+Se llama al script func, que se encuentra en un archivo separado llamado func.m
+Se le da un rango a la función fzero para que calcule la raíz de la función
+dentro del dominio válido [1,19] ya que en estos valores la función es
+válida y no se indefine.
+================================================================================
+#}
 d_star = fzero(@func, [1, 19]);
 fprintf('Distancia estimada: d* = %.10f\n', d_star);
