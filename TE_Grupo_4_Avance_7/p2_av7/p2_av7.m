def40d573d45cd611ebbbd1260d8cd74977ae799
Mauro Navarro <mauronav006@gmail.com>
2026-06-21 16:02:31 -0600

    feat: Added iterative method to solve the gaussian quadrature. This one has an internal tolerance and upscales the amount of subintervals until it reaches a max amount of 100 000 000.

--- TE_Grupo_4_Avance_7/p2_av7/p2_av7.m
+++ TE_Grupo_4_Avance_7/p2_av7/p2_av7.m
@@ -1,19 +1,21 @@
 function p2_av7()
-  f = @(x) x^4;
-  a = 1;
-  b = 3;
-  k = 3;
-  n = 10;
+  f = @(x) cos(x).*exp(x);
+  a = 2;
+  b = 5;
+  k = 7;
+  n = 20;
 
-  I = cuad_gauss(f, a, b, k)
+  Isimple = cuad_gauss(f, a, b, k)
 
-  I = cuad_gauss_comp(f, a, b, k, n)
+  Icompuesto = cuad_gauss_comp(f, a, b, k, n)
+
+  [Iiterativa, err, n] = cuad_gauss_iter(f, a, b, k)
 
   % ============================================================================
-  % Funcionn para calcular cuadratura gaussiana simple
+  % Funcion para calcular cuadratura gaussiana simple
   % ============================================================================
 
-  function I = cuad_gauss(f, a, b, k)
+  function Is = cuad_gauss(f, a, b, k)
 
   % Traemos los valores de x y w previamente computados (se extraen del repositorio de git indicado).
   % Valores se pueden observar en el archivo nodos_pesos.m .
@@ -24,27 +26,67 @@ function p2_av7()
   t = ((b-a).*x + (b+a)) ./ 2;
 
   % Se calcula la aproximación de la cuadratura gaussiana en el intervalo dado.
-  I = ((b-a)/2) * sum(w .* arrayfun(f, t));
+  Is = ((b-a)/2) * sum(w .* arrayfun(f, t));
 
   endfunction
 
   % ============================================================================
-  % Funcionn para calcular cuadratura gaussiana con método compuesto (subintervalos)
+  % Funcion para calcular cuadratura gaussiana con método compuesto (subintervalos)
   % ============================================================================
 
-  function I = cuad_gauss_comp(f, a, b, k, n)
+  function Ic = cuad_gauss_comp(f, a, b, k, n)
 
     % calculamos h en base a la cantidad de subintervalos
     h = (b-a)/n;
-    I = 0;
+    Ic = 0;
 
     % Hacemos un ciclo for para trabajar calcular la cuadratura gaussiana simple de subintervalo.
     for j = 1:n
       aj = a + (j-1)*h;
       bj = a + j*h;
 
-      I = I + cuad_gauss(f, aj, bj, k);
+      Ic = Ic + cuad_gauss(f, aj, bj, k);
     endfor
 
   endfunction
+
+  % ============================================================================
+  % Funcion para calcular cuadratura gaussiana con método iterativo (con tolerancia)
+  % ============================================================================
+
+  function [I, err, n] = cuad_gauss_iter(f, a, b, k)
+
+    % Se definen los valores de tolerancia y subitervalos máximos de acuerdo al enunciado
+    tol = 1e-8;
+    nmax = 100e6;
+
+    n = 1;
+    I_ant = cuad_gauss_comp(f, a, b, k, n);
+
+    % Se calcula la cuadratura gaussiana commpuesta con intervalos de 2*n por ciclo hasta
+    % llegar a un nivel de tolerancia aceptable o alcanzar el máximo de subintervalos.
+    while n < nmax
+
+      n = 2*n;
+
+      if n > nmax
+        n = nmax;
+      endif
+
+      I = cuad_gauss_comp(f, a, b, k, n);
+
+      err = abs(I - I_ant);
+
+      if err < tol
+        return;
+      endif
+
+      I_ant = I;
+
+    endwhile
+
+    warning("Se alcanzó el número máximo de subintervalos sin cumplir la tolerancia.");
+
+  endfunction
+
 end
